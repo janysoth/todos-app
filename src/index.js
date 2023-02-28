e94035ba224ea9f23c364c13319f00e2fbc8eb3d
Jonny Soth <janysoth@gmail.com>
2023-02-28 00:27:26 -0600

    Create an input and a submit button.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const greeting = 'Hello World';
-(function () {
-    alert(greeting);
-})()
+function myFunction(){
+    let userInput = document.querySelector("#userInput");
+    let message = document.querySelector("#message"); 
+
+    message.innerHTML = `Welcome to my page, ${userInput.value}.`;
+}
 
