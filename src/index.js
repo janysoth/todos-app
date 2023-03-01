e6515d8a638c9365f5fb0b17f5bca86cd562eb4c
Jonny Soth <janysoth@gmail.com>
2023-03-01 00:03:33 -0600

    Made changes to create a new welcome message on the next line.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function introMessage(){
     let userInput = document.querySelector("#userInput");
     let message = document.querySelector("#message"); 
 
-    message.innerHTML = message.innerHTML + `Welcome to my page, ${userInput.value}.`;
+    message.innerHTML = message.innerHTML + `Welcome to my page, ${userInput.value}.<br/>`;
     
 }
 
