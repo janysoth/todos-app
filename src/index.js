b07c20c665af4c2deb0310a52b4f6383d5bd16e0
Jonny Soth <janysoth@gmail.com>
2023-03-02 01:06:01 -0600

    Add a delete button to remove messages.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-function introMessage(){
-    let userInput = document.querySelector("#userInput");
-    let message = document.querySelector("#message"); 
-
-    message.innerHTML = message.innerHTML + `Welcome to my page, ${userInput.value}.<br/>`;
+function addMessage(){
+    const userInput = document.querySelector("#userInput");
+    const message = document.querySelector("#message"); 
     
+    message.innerHTML = message.innerHTML + `Welcome to my page, ${userInput.value}.<br/>`;
+}
+
+function deleteMessage(){ 
+    const userInput = document.querySelector("#userInput");
+    const message = document.querySelector("#message");
+    message.innerHTML = "";
 }
 
