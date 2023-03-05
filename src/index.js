d366e3668e54aab47efd2e004daf949beb8a7af2
Jonny Soth <janysoth@gmail.com>
2023-03-04 22:17:30 -0600

    Add a button to remove message for each line.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-function addMessage(){
-    const userInput = document.querySelector("#userInput");
-    const message = document.querySelector("#message"); 
-    
-    message.innerHTML = message.innerHTML + `Welcome to my page, ${userInput.value}.<br/>`;
+// Create variables
+const divList = document.querySelector('.listHolder');
+const addInput = document.querySelector('#userInput');
+const addBtn = document.querySelector('#addBtn');
+
+// Create a function to create the list
+function addList (){
+    if (addInput.value === '') {
+        alert('Please Enter Your Name.') 
+        return false;
+    }
+
+    const ul = divList.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerHTML = `Welcome to my page, ${addInput.value}.`;
+    addInput.value = ''; // Add this to remove the previous
+    ul.appendChild(li);
+    createBtn(li);
 }
 
-function deleteMessage(){ 
-    const userInput = document.querySelector("#userInput");
-    const message = document.querySelector("#message");
-    message.innerHTML = "";
+addBtn.addEventListener('click', () => {
+    addList();
+})
+
+// Add list when Enter Key pressed
+addInput.addEventListener('keyup', (event) => {
+    if(event.which === 13)
+        addList();
+})
+
+/* 3. create action buttons*/
+
+// create variables
+const listUl = document.querySelector('.list');
+
+// create remove button
+function createBtn(button) {
+  const removeBtn = document.createElement('button');
+  removeBtn.className = 'btn-icon remove';
+  button.appendChild(removeBtn);
+
+  return button;
 }
 
+// To Remove each message line when the button is clicked
+divList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const button = event.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.className === 'btn-icon remove') 
+      ul.removeChild(li);
+  }
+});
